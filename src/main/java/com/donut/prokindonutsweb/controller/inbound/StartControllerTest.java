3986584be0855c11cd24ab2f9c91c36c5cfa4e7a
Cmk1031 <maple1031@naver.com>
2025-04-16 18:18:42 +0900

    fix: mybatis-config.xml 파일 설정

---
+++ src/main/java/com/donut/prokindonutsweb/controller/inbound/StartControllerTest.java
@@ -0,0 +1,7 @@
+package com.donut.prokindonutsweb.controller.inbound;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StartControllerTest {
+}
---
+++ src/main/java/com/donut/prokindonutsweb/service/inbound/StartServiceTest.java
@@ -0,0 +1,7 @@
+package com.donut.prokindonutsweb.service.inbound;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StartServiceTest {
+}
