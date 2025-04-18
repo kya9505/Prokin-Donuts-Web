7b8e435b75cdcc1ca8125bab572d86152d3802b2
Cmk1031 <maple1031@naver.com>
2025-04-18 16:14:43 +0900

    fix: 창고관리자 입고요청 VO 변경

--- src/main/java/com/donut/prokindonutsweb/mappers/inbound/InboundMapper.java
+++ src/main/java/com/donut/prokindonutsweb/mappers/inbound/InboundMapper.java
@@ -2,13 +2,14 @@ package com.donut.prokindonutsweb.mappers.inbound;
 
 import com.donut.prokindonutsweb.dto.inbound.InboundDTO;
 import com.donut.prokindonutsweb.vo.inbound.InboundDetailVO;
+import com.donut.prokindonutsweb.vo.inbound.InboundVO;
 import com.donut.prokindonutsweb.vo.inbound.ProductVO;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface InboundMapper {
     List<ProductVO> selectAllProductList();
-    void insertInbound(InboundDTO inboundDTO);
+    void insertInbound(InboundVO inboundVO);
     // 입고 상세 정보
     void insertInboundDetailList(@Param("list") List<InboundDetailVO> list);
 }
