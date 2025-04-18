2eb21e9f30842aa6354852700dafa7909402b5e3
Cmk1031 <maple1031@naver.com>
2025-04-18 21:13:21 +0900

    temp

--- src/main/java/com/donut/prokindonutsweb/controller/inbound/InboundController.java
+++ src/main/java/com/donut/prokindonutsweb/controller/inbound/InboundController.java
@@ -3,6 +3,7 @@ package com.donut.prokindonutsweb.controller.inbound;
 import com.donut.prokindonutsweb.dto.inbound.ProductDTO;
 import com.donut.prokindonutsweb.service.inbound.InboundService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
