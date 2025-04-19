f902d68527f8c811bebf8617a0a1364d8d1011c1
kya <dbsdk4211@naver.com>
2025-04-19 18:41:03 +0900

    feat : 본사관리자 회원 등록/수정/삭제 Mapper 구현 및 단위 테스트

--- src/main/java/com/donut/prokindonutsweb/mappers/member/MemberMapper.java
+++ src/main/java/com/donut/prokindonutsweb/mappers/member/MemberMapper.java
@@ -2,10 +2,14 @@ package com.donut.prokindonutsweb.mappers.member;
 
 
 import com.donut.prokindonutsweb.vo.member.MemberAccountVO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberMapper{
     List<MemberAccountVO> selectMember();
+    void insertMember(MemberAccountVO memberVO);
+    void updateMember(@Param("list") List<MemberAccountVO> memberVOList);
+    void deleteMember(String memberCode);
 }
--- src/test/java/com/donut/prokindonutsweb/mappers/member/MemberMapperTest.java
+++ src/test/java/com/donut/prokindonutsweb/mappers/member/MemberMapperTest.java
@@ -3,12 +3,14 @@ package com.donut.prokindonutsweb.mappers.member;
 import com.donut.prokindonutsweb.vo.member.MemberAccountVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
@@ -28,4 +30,43 @@ class MemberMapperTest {
         memberList.forEach(log::info);
     }
 
+    @Test
+    @DisplayName("회원등록 DB 반영 테스트")
+    public void insertMember(){
+        MemberAccountVO memberAccountVO = MemberAccountVO.builder().memberCode("QH100")
+                .authorityCode("QH")
+                .name("100")
+                .phoneNumber("100")
+                .email("100")
+                .address("100")
+                .id("100")
+                .password("100").build();
+
+        mapper.insertMember(memberAccountVO);
+    }
+
+    @Test
+    @DisplayName("회원수정 DB 반영 테스트")
+    public void updateMember(){
+        MemberAccountVO memberAccountVO = MemberAccountVO.builder()
+                .memberCode("QH100")
+                .authorityCode("QH")
+                .name("200")
+                .phoneNumber("200")
+                .email("200")
+                .address("200")
+                .id("200")
+                .password("200").build();
+
+        List<MemberAccountVO> list = new ArrayList<>();
+        list.add(memberAccountVO);
+
+        mapper.updateMember(list);
+    }
+
+    @Test
+    @DisplayName("회원 삭제 DB 반영 테스트")
+    public void deleteMember(){
+        mapper.deleteMember("QH100");
+    }
 }
