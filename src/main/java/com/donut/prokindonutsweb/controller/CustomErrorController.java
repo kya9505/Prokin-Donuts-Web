89ea6aa838f216d14fb5891a0f623ef33de0d84d
Cmk1031 <maple1031@naver.com>
2025-04-19 12:55:21 +0900

    feat: 에러 화면 페이지 설정

--- src/main/java/com/donut/prokindonutsweb/controller/CustomErrorController.java
+++ src/main/java/com/donut/prokindonutsweb/controller/CustomErrorController.java
@@ -2,6 +2,7 @@ package com.donut.prokindonutsweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -10,15 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/error")
 public class CustomErrorController {
-    @RequestMapping("/404")
-    public String error404(HttpServletRequest request, Model model) {
+    @GetMapping("/404")
+    public String error404(Model model) {
         model.addAttribute("statusCode", 404);
         model.addAttribute("message", "페이지를 찾을 수 없습니다.");
         return "error/404"; // -> /WEB-INF/views/error/404.jsp
     }
 
-    @RequestMapping("/500")
-    public String error500(HttpServletRequest request, Model model) {
+    @GetMapping("/500")
+    public String error500(Model model) {
         model.addAttribute("statusCode", 500);
         model.addAttribute("message", "서버 오류가 발생했습니다.");
         return "error/500";
