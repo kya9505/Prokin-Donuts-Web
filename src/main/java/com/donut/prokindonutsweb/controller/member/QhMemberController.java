92708128e70215068143f9a6ce4d3f28e72ae8f7
kya <dbsdk4211@naver.com>
2025-04-20 01:09:16 +0900

    feat : qh add/update/delete 기능 controller 구현 및 테스트

---
+++ src/main/java/com/donut/prokindonutsweb/controller/member/QhMemberController.java
@@ -0,0 +1,48 @@
+package com.donut.prokindonutsweb.controller.member;
+
+import com.donut.prokindonutsweb.dto.member.MemberAccountDTO;
+import com.donut.prokindonutsweb.dto.warehouse.WarehouseDTO;
+import com.donut.prokindonutsweb.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/qh/member")
+public class QhMemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/list")
+    public void qhGetMemberList(Model model){
+        List<MemberAccountDTO> qhMemberList = memberService.findMember().orElse(Collections.emptyList());
+        model.addAttribute("qhMemberList",qhMemberList);
+    }
+
+    @PostMapping("/add")
+    public String qhAddMemberList(MemberAccountDTO memberAccountDTO) {
+        memberService.saveMember(memberAccountDTO);
+        return "redirect:/qh/member/list";
+    }
+
+    @PostMapping("/update")
+    public String qhUpdateMembers(List<MemberAccountDTO> memberlist) {
+        memberService.updateMember(memberlist);
+        return "redirect:/qh/member/list";
+    }
+
+    @PostMapping("/delete")
+    public String qhDeleteMembers(List<String> memberCode) {
+        memberService.deleteMember(memberCode);
+        return "redirect:/qh/member/list";
+    }
+}
--- src/test/java/com/donut/prokindonutsweb/controller/member/MemberControllerTest.java
+++ src/test/java/com/donut/prokindonutsweb/controller/member/MemberControllerTest.java
@@ -1,8 +1,8 @@
 package com.donut.prokindonutsweb.controller.member;
 
 import com.donut.prokindonutsweb.dto.member.MemberAccountDTO;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +11,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 @Log4j2
@@ -21,27 +21,71 @@ import static org.junit.jupiter.api.Assertions.*;
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
 class MemberControllerTest {
     @Autowired
-    private MemberController memberController;
+    private WmMemberController wmMemberController;
+    @Autowired
+   private QhMemberController qhMemberController;
 
     /*
     * memberController의 wmMemberList 테스트
     * Model객체에 담긴 List 확인
     * */
     @Test
-    public void wmMemberListTest(){
+    @DisplayName("wm 회원 목록 Model 바인딩 테스트")
+    public void wmGetMemberList(){
         Model model = new ExtendedModelMap(); //model은 interface이므로 객체생성 불가, 구현체 주입
-        memberController.wmGetMember(model);
+        wmMemberController.wmGetMember(model);
         List<MemberAccountDTO> memberList = (List<MemberAccountDTO>)model.getAttribute("wmMemberList");
         assertNotNull(memberList);
         memberList.forEach(log::info);
     }
 
     @Test
-    public void qhMemberListTest(){
+    @DisplayName("qh 회원 목록 Model 바인딩 테스트")
+    public void qhGetMemberList(){
         Model model = new ExtendedModelMap(); //model은 interface이므로 객체생성 불가, 구현체 주입
-        memberController.qhGetMemberList(model);
+        qhMemberController.qhGetMemberList(model);
         List<MemberAccountDTO> memberList = (List<MemberAccountDTO>)model.getAttribute("qhMemberList");
         assertNotNull(memberList);
         memberList.forEach(log::info);
+    }
+
+    @Test
+    @DisplayName("Controller 회원 등록 DB 반영 테스트")
+    public void qhAddMemberList(){
+        MemberAccountDTO memberDTO = MemberAccountDTO.builder().memberCode("QH100")
+                .authorityCode("QH")
+                .name("100")
+                .phoneNumber("100")
+                .email("100")
+                .address("100")
+                .id("100")
+                .password("100").build();
+        qhMemberController.qhAddMemberList(memberDTO);
+    }
+
+    @Test
+    @DisplayName("Controller 회원 수정 DB 반영 테스트")
+    public void qhUpdateMembers(){
+        MemberAccountDTO memberDTO = MemberAccountDTO.builder()
+                .memberCode("QH100")
+                .authorityCode("QH")
+                .name("200")
+                .phoneNumber("200")
+                .email("200")
+                .address("200")
+                .id("200")
+                .password("200").build();
+        List<MemberAccountDTO> list = new ArrayList<>();
+        list.add(memberDTO);
+
+        qhMemberController.qhUpdateMembers(list);
+
+    }
+
+    @Test
+    @DisplayName("Controller 회원 삭제 DB 반영 테스트")
+    public void qhDeleteMembers(){
+        List<String> memberCodeList = new ArrayList<>(Arrays.asList("QH100"));
+        qhMemberController.qhDeleteMembers(memberCodeList);
     }
 }
