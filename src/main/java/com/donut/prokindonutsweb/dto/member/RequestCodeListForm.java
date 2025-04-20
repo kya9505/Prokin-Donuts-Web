91bad340d196729ef1e4da3d74e52d1dab693d1f
kya <dbsdk4211@naver.com>
2025-04-21 00:25:41 +0900

    feat : 본사관리자의 회원요청목록 조회/승인 Controller 구현

---
+++ src/main/java/com/donut/prokindonutsweb/dto/member/RequestCodeListForm.java
@@ -0,0 +1,10 @@
+package com.donut.prokindonutsweb.dto.member;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RequestCodeListForm {
+    private List<String> requestList;
+}
