f6346ec143fdae8c5c6640548fec428e72aa428f
kya <dbsdk4211@naver.com>
2025-04-21 23:57:08 +0900

    feat :  회원가입 요청 Controller 구현 및 단위 테스트

---
+++ src/main/java/com/donut/prokindonutsweb/login/controller/SignupController.java
@@ -0,0 +1,53 @@
+package com.donut.prokindonutsweb.login.controller;
+
+import com.donut.prokindonutsweb.login.service.SignupService;
+import com.donut.prokindonutsweb.member.dto.MemberAccountDTO;
+import com.donut.prokindonutsweb.member.dto.MemberRequestDTO;
+import com.donut.prokindonutsweb.member.mapper.MemberMapper;
+import com.donut.prokindonutsweb.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class SignupController {
+    private final SignupService signupService;
+    private final MemberService memberService;
+
+    @GetMapping("/home/signup")
+    public String signup(Model model){
+        return "redirect:home/login";
+    }
+
+    @PostMapping("/home/signup")
+    public String signup(@Valid @ModelAttribute("memberRequestDTO")MemberRequestDTO memberRequestDTO,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes){
+// 유효성 검사 실패 시, 첫 번째 에러 메시지와 입력 값을 플래시 속성으로 전달하고 목록 페이지로 리다이렉트
+        if (bindingResult.hasErrors()) {
+            String errorMessage = bindingResult.getAllErrors().get(0).getDefaultMessage();
+            redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
+            redirectAttributes.addFlashAttribute("memberAccountDTO", memberRequestDTO);
+            return "redirect:list";
+        }
+        signupService.saveMemberRequset(memberRequestDTO);
+
+        return "redirect:home/login";
+    }
+
+    @GetMapping(value ="/check",  produces = "text/plain")
+    @ResponseBody
+    public String checkId(@RequestParam("id") String id){
+        return memberService.memberIdCheck(id)? "true" : "false";
+    }
+
+
+}
---
+++ src/test/java/com/donut/prokindonutsweb/login/controller/SignupControllerTest.java
@@ -0,0 +1,41 @@
+package com.donut.prokindonutsweb.login.controller;
+
+import com.donut.prokindonutsweb.member.dto.MemberAccountDTO;
+import com.donut.prokindonutsweb.member.dto.MemberRequestDTO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
+class SignupControllerTest {
+@Autowired
+private SignupController signupController;
+
+    @Test
+    @DisplayName("signup Controller 회원가입 요청 DB 반영 테스트")
+    void signup() {
+        MemberRequestDTO dto = MemberRequestDTO.builder()
+                .id("test3")
+                .email("test3")
+                .name("test3")
+                .address("test3")
+                .phoneNumber("test3")
+                .password("test3")
+                .build();
+        BindingResult bindingResult = new BeanPropertyBindingResult(dto, "MemberRequestDTO");
+        RedirectAttributes redirectAttributes = new RedirectAttributesModelMap();
+        signupController.signup(dto,bindingResult,redirectAttributes);
+
+    }
+}
