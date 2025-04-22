1843c77cbed3f893bd6dcaf705669e457a8f7c5e
kya <dbsdk4211@naver.com>
2025-04-23 00:48:47 +0900

    refactor : 인증번호 발송 form 추가

--- src/main/java/com/donut/prokindonutsweb/login/service/FindService.java
+++ src/main/java/com/donut/prokindonutsweb/login/service/FindService.java
@@ -3,14 +3,16 @@ package com.donut.prokindonutsweb.login.service;
 import com.donut.prokindonutsweb.login.dto.VerificationCodeDTO;
 import com.donut.prokindonutsweb.member.dto.MemberAccountDTO;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
+import java.io.FileNotFoundException;
 import java.util.Optional;
 
 public interface FindService {
     Optional<MemberAccountDTO> findEmail(String inputEmail);
     Optional<MemberAccountDTO> findId(String inputId);
-    void sendEmail(String toEmail,VerificationCodeDTO code);
+    void sendEmail(String toEmail,VerificationCodeDTO code) throws MessagingException, FileNotFoundException;
     VerificationCodeDTO randomCode();
 
-   void saveCodeToSession(MemberAccountDTO member,String email, HttpSession session);
+   void saveCodeToSession(MemberAccountDTO member,String email, HttpSession session) throws MessagingException, FileNotFoundException;
 }
