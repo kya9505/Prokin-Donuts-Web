6cb39cfb999a9d577a03815e5ce173a767c53f99
kya <dbsdk4211@naver.com>
2025-04-22 19:22:38 +0900

    feat : 아이디찾기/이메일발송/인증번호 발급 등의 기능 구현 및 단위테스트

---
+++ src/main/java/com/donut/prokindonutsweb/login/service/FindServiceImpl.java
@@ -0,0 +1,76 @@
+package com.donut.prokindonutsweb.login.service;
+
+import com.donut.prokindonutsweb.login.dto.VerificationCodeDTO;
+import com.donut.prokindonutsweb.login.mapper.FindIdMapper;
+import com.donut.prokindonutsweb.member.dto.MemberAccountDTO;
+import com.donut.prokindonutsweb.member.vo.MemberAccountVO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class FindServiceImpl implements FindService {
+    private final ModelMapper modelMapper;
+    private final JavaMailSender mailSender;
+    private final FindIdMapper findIdMapper;
+
+    /*이메일로 객체 찾기
+    */
+    @Override
+    public Optional<MemberAccountDTO> findId(String email) {
+        MemberAccountVO memberAccountVO = findIdMapper.searchEmail(email);
+        MemberAccountDTO memberAccountDTO = modelMapper.map(memberAccountVO,MemberAccountDTO.class);
+        return Optional.ofNullable(memberAccountDTO);
+    }
+
+
+    //아이디로 객체 찾기
+    @Override
+    public Optional<MemberAccountDTO> findPassword(String id){
+        return null;
+    }
+
+    //이메일발송
+    @Override
+    public void sendEmail(String email,VerificationCodeDTO code) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(email);
+        mail.setSubject("ProkinDouts 아이디/비밀번호 찾기 인증번호 입니다.");
+        mail.setText("인증번호 : " + code.getCode() + "\n 해당 인증번호를 아이디/비밀번호찾기 화면에 입력해주세요.");
+        mail.setFrom("prokindonuts@gmail.com");
+        mailSender.send(mail);
+        log.info("전송 완료 (받는사람: {})", email);
+    }
+
+    //6자리의 랜덤한 인증번호 생성하여 적용
+    @Override
+    public VerificationCodeDTO randomCode() {
+         String randomCode = String.valueOf((int)(Math.random()*900000) + 100000);
+         //10분 후 만료되는 인증번호
+         VerificationCodeDTO verificationCode = VerificationCodeDTO.builder()
+                 .code(randomCode)
+                 .expireTime(LocalDateTime.now().plusMinutes(10))
+                 .build();
+         return verificationCode;
+    }
+
+    //코드와 이메일을 세션에 저장
+    public void saveCodeToSession(MemberAccountDTO member,String email ,HttpSession session) {
+        VerificationCodeDTO code = randomCode();
+        sendEmail(email,code);
+        session.setAttribute("authCode", code);
+        session.setAttribute("authEmail", email);
+        session.setAttribute("member",member);
+    }
+
+}
---
+++ src/test/java/com/donut/prokindonutsweb/login/service/FindIdServiceTest.java
@@ -0,0 +1,97 @@
+package com.donut.prokindonutsweb.login.service;
+
+import com.donut.prokindonutsweb.login.dto.VerificationCodeDTO;
+import com.donut.prokindonutsweb.login.mapper.FindIdMapper;
+import com.donut.prokindonutsweb.member.dto.MemberAccountDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@RequiredArgsConstructor
+public class FindIdServiceTest {
+    @Autowired
+    private FindService findService;
+
+    @Test
+    @DisplayName("이메일로 회원찾기")
+    public void findId() {
+        MemberAccountDTO member = findService.findId("fire123@gmail.com").orElseThrow();
+        assertNotNull(member);
+        log.info(member);
+    }
+
+    @Test
+    public void findPassword() {
+    }
+
+    @Test
+    @DisplayName("이메일 발송 테스트")
+    public void sendEmail() {
+        //30초 후 인증번호 만료
+        VerificationCodeDTO code = VerificationCodeDTO.builder().code("12345").build();
+        findService.sendEmail("dbsdk4211@naver.com", code);
+    }
+
+    @Test
+    @DisplayName("6자리 랜덤한 코드 생성, 유효시간 10분 테스트")
+    public void randomCode() {
+        VerificationCodeDTO verificationCode = findService.randomCode();
+        assertNotNull(verificationCode);
+        log.info(verificationCode);
+
+// Duration 계산
+        long minutes = Duration.between(LocalDateTime.now(), verificationCode.getExpireTime()).toMinutes();
+// 10분 이내 유효성 검증
+        assertTrue(minutes <= 10 && minutes > 0);
+    }
+
+
+    @Test
+    @DisplayName("생성된 코드와 발송 이메일, 회원객체 세션저장 테스트")
+    public void testSaveCodeToSession() {
+
+        MemberAccountDTO member = MemberAccountDTO.builder()
+                .id("test123")
+                .email("dbsdk4211@naver.com")
+                .name("홍길동")
+                .build();
+
+        String email = member.getEmail();
+        //목세션
+        MockHttpSession session = new MockHttpSession();
+
+
+        findService.saveCodeToSession(member, email, session);
+
+        // then
+        VerificationCodeDTO authCode = (VerificationCodeDTO) session.getAttribute("authCode");
+        String authEmail = (String) session.getAttribute("authEmail");
+        MemberAccountDTO sessionMember = (MemberAccountDTO) session.getAttribute("member");
+
+        assertNotNull(authCode);
+        assertEquals(email, authEmail);
+        assertEquals(member, sessionMember);
+
+        VerificationCodeDTO code = (VerificationCodeDTO) authCode;
+        assertFalse(code.isExpired()); // 생성 직후엔 당연히 유효해야 함
+    }
+}
+
+
+
