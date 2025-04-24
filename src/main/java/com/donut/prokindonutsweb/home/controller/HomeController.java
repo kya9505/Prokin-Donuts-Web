d1477f132426aaf3627c67a99b6621d82574dd74
kya <dbsdk4211@naver.com>
2025-04-24 23:31:01 +0900

    git pull origin dev && merge

--- src/main/java/com/donut/prokindonutsweb/home/controller/HomeController.java
+++ src/main/java/com/donut/prokindonutsweb/home/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.donut.prokindonutsweb.homeController;
+package com.donut.prokindonutsweb.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
