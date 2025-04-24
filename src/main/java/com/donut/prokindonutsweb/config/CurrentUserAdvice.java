b8a365017d2116063fcc7a5bae2a37d40ca47a3a
kya <dbsdk4211@naver.com>
2025-04-24 19:10:21 +0900

    fix :

--- src/main/java/com/donut/prokindonutsweb/config/CurrentUserAdvice.java
+++ src/main/java/com/donut/prokindonutsweb/config/CurrentUserAdvice.java
@@ -1,21 +1,25 @@
 package com.donut.prokindonutsweb.config;
 
+import com.donut.prokindonutsweb.member.vo.MemberAccountVO;
 import com.donut.prokindonutsweb.security.dto.CustomUserDetails;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
+@Log4j2
+
+/**
+ * 모든 컨트롤러가 실행될 때마다,
+ * model에 "member"라는 이름으로 현재 로그인 정보를 담아줍니다.
+ */
 public class CurrentUserAdvice {
 
-    /**
-     * 모든 컨트롤러가 실행될 때마다,
-     * model에 "user"라는 이름으로 현재 로그인 정보를 담아줍니다.
-     */
-    @ModelAttribute("user")
-    public CustomUserDetails currentUser(
-            @AuthenticationPrincipal CustomUserDetails user
-    ) {
-        return user;
+    //jsp에서 'member'로 attribute
+    @ModelAttribute("member")
+    public MemberAccountVO currentUser(@AuthenticationPrincipal CustomUserDetails user) {
+        return user.getMember();
     }
 }
+
