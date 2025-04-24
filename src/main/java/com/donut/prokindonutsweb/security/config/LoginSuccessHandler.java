ca7abba0fca5612153c7e04985215deb95341894
kya <dbsdk4211@naver.com>
2025-04-24 19:21:16 +0900

    fix : 시큐리티 세션 저장기능이 안되서 세션에 직접 저장, 추후 refactor예정

--- src/main/java/com/donut/prokindonutsweb/security/config/LoginSuccessHandler.java
+++ src/main/java/com/donut/prokindonutsweb/security/config/LoginSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.donut.prokindonutsweb.security.config;
 
+import com.donut.prokindonutsweb.security.dto.CustomUserDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -13,10 +14,16 @@ import java.util.Collection;
 
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        // 로그인한 객체를 세션에 저장
+        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
+        request.getSession().setAttribute("member", principal.getMember());
+
         //Authentication에서 권한 목록(Authorities) 가져오기 (UserDetails가 권한을 List로 관리)
             Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
         //기본 경로(권한 없을 시)
             String redirectUrl ="/";
         //권한 별 메인 화면 으로 이동
