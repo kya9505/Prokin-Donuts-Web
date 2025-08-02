bff3e0dbb876cf569eb9435816849bcc01909fca
kya <dbsdk4211@naver.com>
2025-08-02 23:58:44 +0900

    feat : 발주목록 컨트롤러

---
+++ src/main/java/com/donut/prokindonutsweb/order/controller/OrderListController.java
@@ -0,0 +1,54 @@
+package com.donut.prokindonutsweb.order.controller;
+
+import com.donut.prokindonutsweb.order.dto.OrderDTO;
+import com.donut.prokindonutsweb.order.dto.OrderDetailDTO;
+import com.donut.prokindonutsweb.order.service.OrderService;
+import com.donut.prokindonutsweb.security.dto.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/fm")
+public class OrderListController {
+
+    private final OrderService orderService;
+
+
+    @GetMapping("/list")
+    public String getOrderList(Model model, @AuthenticationPrincipal CustomUserDetails user) {
+        log.info("getOrderList() called");
+
+        String memberCode = user.getMemberCode();
+        log.info("memberCode: {}", memberCode);
+        String franchiseCode = orderService.findFranchiseCode(memberCode);
+
+        List<OrderDTO> orderList = orderService.findOrderList(franchiseCode);
+        model.addAttribute("orderList", orderList);
+
+        return "/fm/list";
+    }
+
+    @GetMapping("/list/detail")
+    public String getOrderDetailList(Model model, @AuthenticationPrincipal CustomUserDetails user) {
+        log.info("getOrderDetailList() called");
+
+        String memberCode = user.getMemberCode();
+        log.info("memberCode: {}", memberCode);
+        String franchiseCode = orderService.findFranchiseCode(memberCode);
+
+        List<OrderDetailDTO> orderDetailList = orderService.findOrderDetailList(franchiseCode);
+        model.addAttribute("orderDetailList", orderDetailList);
+
+        return "/fm/list";
+    }
+}
