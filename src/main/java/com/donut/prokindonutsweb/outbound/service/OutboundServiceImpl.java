faf9d5b3e3a461a6741598faea03187ebdfea344
kya <dbsdk4211@naver.com>
2025-08-03 22:22:52 +0900

    feat : 출고요청 승인 service  구현 및 test - 출고요청 목록 조회 - 출고승인 시 상태 변경 - 재고 존재 확인 - 재고 반영 (차감)

--- src/main/java/com/donut/prokindonutsweb/outbound/service/OutboundServiceImpl.java
+++ src/main/java/com/donut/prokindonutsweb/outbound/service/OutboundServiceImpl.java
@@ -3,20 +3,29 @@ package com.donut.prokindonutsweb.outbound.service;
 import com.donut.prokindonutsweb.outbound.dto.OutboundDTO;
 import com.donut.prokindonutsweb.outbound.mapper.OutboundMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OutboundServiceImpl implements OutboundService{
 
     private final OutboundMapper outboundMapper;
 
+    //출고목록 조회
     @Override
     public List<OutboundDTO> findOutboundList(String warehouseCode) {
         return outboundMapper.selectAllOutboundList(warehouseCode);
 
+    }
+
+    //출고 요청 목록 조회
+    @Override
+    public List<OutboundDTO> findApprovalOutboundList(String warehouseCode){
+        return outboundMapper.selectApprovalOutboundList(warehouseCode);
     }
 
 
@@ -26,6 +35,7 @@ public class OutboundServiceImpl implements OutboundService{
         return outboundMapper.checkInventory(outboundCode);
     }
 
+    //출고승인
     @Override
     public void approveOutbound(String outboundCode) {
         outboundMapper.approveOutbound(outboundCode);
@@ -37,6 +47,7 @@ public class OutboundServiceImpl implements OutboundService{
         outboundMapper.updateInventory(outboundCode);
     }
 
+    //창고코드 조회
     @Override
     public String getWarehouseCode(String memberCode) {
         return outboundMapper.getWarehouseCode(memberCode);
