4abd48def5d0da443dae05a2da0d82ce5864691c
psns <psns0122@naver.com>
2025-08-07 03:16:07 +0900

    feat: 매월 1일 00시 적정재고량 재설정 알림 메일 자동 발송 스케듈러 설정

---
+++ src/main/java/com/donut/prokindonutsweb/scheduler/dto/ManagerWarehouseInfoDTO.java
@@ -0,0 +1,10 @@
+package com.donut.prokindonutsweb.scheduler.dto;
+
+import lombok.Data;
+
+@Data
+public class ManagerWarehouseInfoDTO {
+  private String email;
+  private String managerName;
+  private String warehouseName;
+}
