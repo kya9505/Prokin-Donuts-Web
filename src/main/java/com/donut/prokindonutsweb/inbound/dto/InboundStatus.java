8c72f08a9b3f1e2b459d6a894d8fb1b81e017354
Cmk1031 <maple1031@naver.com>
2025-08-08 01:11:52 +0900

    refactor: 입고상태 enum 값 추가

--- src/main/java/com/donut/prokindonutsweb/inbound/dto/InboundStatus.java
+++ src/main/java/com/donut/prokindonutsweb/inbound/dto/InboundStatus.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum InboundStatus {
     REQUEST("입고요청"),
-    APPROVE("승인대기");
+    APPROVE("승인대기"),
+    CANCEL("입고취소"),
+    COMPLETE("입고완료");
 
     private final String status;
 
